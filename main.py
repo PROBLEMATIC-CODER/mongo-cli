3f611cfcf80b262dd6afedf3642dc89bc34f1adc
Shashank <xcoder236@gmail.com>
2023-08-02 09:15:58 +0530

    Initial commit

--- main.py
+++ main.py
@@ -385,28 +385,28 @@ def format_document(document):
 
 
 def get_collection_data():
-    # try:
-    if(collection != None and db != None):
-        documents = db[current_collection].find()
-        data = []
-        document_count = collection.count_documents({})
-        if(document_count > 0):
-            print(
-                GREEN+f"\nDocuments of collection {current_collection} is given below - \n" + RESET)
-            for document in documents:
-                formatted_data = format_document(document)
-                print(PURPLE + f'\n{formatted_data}'+RESET)
-            return True
-        else:
-            print(
-                YELLOW + '\nCollection is empty!, you can use "insert" command for inserting documents' + RESET)
+    try:
+        if(collection != None and db != None):
+            documents = db[current_collection].find()
+            data = []
+            document_count = collection.count_documents({})
+            if(document_count > 0):
+                print(
+                    GREEN+f"\nDocuments of collection {current_collection} is given below - \n" + RESET)
+                for document in documents:
+                    formatted_data = format_document(document)
+                    print(PURPLE + f'\n{formatted_data}'+RESET)
+                return True
+            else:
+                print(
+                    YELLOW + '\nCollection is empty!, you can use "insert" command for inserting documents' + RESET)
 
-    else:
-        print(RED+f"\nERROR: Invalid process of reading documents"+RESET)
+        else:
+            print(RED+f"\nERROR: Invalid process of reading documents"+RESET)
+            return False
+    except:
+        print(RED + "\nUnable to read collection documents" + RESET)
         return False
-    # except:
-    #     print(RED + "\nUnable to read collection documents" + RESET)
-    #     return False
 
 
 def delete_documents():
